ff4ee6bad15904f7fc40d93a4b4d4f11a4d4a30d
Praveena Kunche <praveenakunche@Aveenas-MacBook-Air.local>
2024-07-29 21:15:38 -0700

    Removed unnecessary code

--- cypress/e2e/multi-tabs/multi-tabs-puppeteer.cy copy.js
+++ cypress/e2e/multi-tabs/multi-tabs-puppeteer.cy copy.js
@@ -1,15 +1,10 @@
 describe("Handle New Tab Opening", () => {
-    /* 
-      
-    
-    */
-  it("should handle new tab opening", () => {
+  it("should handle new tab opening in the same tab", () => {
     cy.visit("https://www.grubhub.com/");
     cy.contains("h1", "Get food delivery and more").should("be.visible");
-
-    cy.multiTab();
+    cy.multiTab1();
     cy.get(".c-button").first().click()
-   // cy.multiTab2();
+    cy.multiTab2();
     cy.contains("h4", "Get good food on the go").should("be.visible");
   });
 });
